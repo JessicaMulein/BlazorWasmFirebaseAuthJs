1f402e2678500afc91c3c6a2743baaf5d52e8a02
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-05-30 01:50:54 -0700

    comparmentalize firebaseJs

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  // The entry point file described above
-  entry: './src/firebase-js.js',
-  // The location of the build folder described above
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'firebase-js-bundle.js'
-  },
-  // Optional and for development only. This provides the ability to
-  // map the built code back to the original source format when debugging.
-  devtool: 'eval-source-map',
+    // The entry point file described above
+    entry: path.resolve(__dirname, './src/firebaseJs.js'),
+    // The location of the build folder described above
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'firebaseJs-bundle.js',
+        library: "$",
+        libraryTarget: "umd",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+        ],
+    },
+    mode: "development",
+    // Optional and for development only. This provides the ability to
+    // map the built code back to the original source format when debugging.
+    devtool: 'eval-source-map',
 };
